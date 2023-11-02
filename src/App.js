cbd8aae6b9c217df7e210407b0e2c115f4cdd5ec
“Dipali <dtank150@rku.ac.in>
2023-11-02 12:43:26 +0530

    travell-list

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App(){
 
   const [items, setItems] = useState([]);
 
+
   function handleAddItems(item){
     setItems((items) => [...items, item]);
   }
@@ -23,11 +24,16 @@ export default function App(){
     setItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed}: item));
   }
 
+  function handleClearList(){
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+    if (confirmed) setItems([]);
+  }
+
   return <div className="app">
     <Logo/>
     <Form onAddItems={handleAddItems}/>
-    <PackingList items={items} onDeleteItem={handleDeleteItems} onToggleItem={handleToggleItem}/>
-    <Stats/>
+    <PackingList items={items} onDeleteItem={handleDeleteItems} onToggleItem={handleToggleItem} onClearList={handleClearList}/>
+    <Stats items={items}/>
   </div>
 }
 
@@ -68,14 +74,30 @@ function handleSubmit(e){
   );
 }
 
-function PackingList({items, onDeleteItem, onToggleItem}){
+function PackingList({items, onDeleteItem, onToggleItem, onClearList}){
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if(sortBy === "input") sortedItems = items;
+  if(sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if(sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
     <ul>
-      {items.map((item) =>(
+      {sortedItems.map((item) =>(
       <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>
       ))}
   </ul>
+  <div className="actions">
+    <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+      <option value='input'>Sort by input order</option>
+      <option value='description'>Sort by description</option>
+      <option value="packed">Sort by packed status</option>
+    </select>
+    <button onClick={onClearList}>Clear List</button>
+  </div>
 </div>
   );
 }
@@ -92,10 +114,21 @@ function Item({item, onDeleteItem, onToggleItem}){
   ); 
 }
 
-function Stats(){
+function Stats({items}){
+  if(!items.length)
+    return(
+    <p className="stats">
+      <em>Start adding some items to your packing list 🚀</em>
+    </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return(
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100 ? 'You got everything! Ready to go ✈️' : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+       </em>
     </footer>
   );
 }
